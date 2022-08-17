edc3389ebc21e6e50197d8c0118ba91f5c6f557d
Ben <benjamin.andremicolon@gmail.com>
2022-08-17 09:56:36 +0300

    fix: close button styling

--- src/index.tsx
+++ src/index.tsx
@@ -23,26 +23,29 @@ export const EmbedPDF: React.FC<Props> = ({ children }) => {
       {shouldDisplayModal &&
         createPortal(
           <div className="simplePDF_container" aria-modal="true">
-            <button
-              onClick={handleCloseModal}
-              className="simplePDF_close"
-              aria-label="Close PDF editor modal"
-            >
-              <svg
-                height="512"
-                viewBox="0 0 512 512"
-                width="512"
-                xmlSpace="preserve"
-                xmlns="http://www.w3.org/2000/svg"
+            <div className="simplePDF_content">
+              <button
+                onClick={handleCloseModal}
+                className="simplePDF_close"
+                aria-label="Close PDF editor modal"
               >
-                <path d="M443.6 387.1 312.4 255.4l131.5-130c5.4-5.4 5.4-14.2 0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4-3.7 0-7.2 1.5-9.8 4L256 197.8 124.9 68.3c-2.6-2.6-6.1-4-9.8-4-3.7 0-7.2 1.5-9.8 4L68 105.9c-5.4 5.4-5.4 14.2 0 19.6l131.5 130L68.4 387.1c-2.6 2.6-4.1 6.1-4.1 9.8 0 3.7 1.4 7.2 4.1 9.8l37.4 37.6c2.7 2.7 6.2 4.1 9.8 4.1 3.5 0 7.1-1.3 9.8-4.1L256 313.1l130.7 131.1c2.7 2.7 6.2 4.1 9.8 4.1 3.5 0 7.1-1.3 9.8-4.1l37.4-37.6c2.6-2.6 4.1-6.1 4.1-9.8-.1-3.6-1.6-7.1-4.2-9.7z" />
-              </svg>
-            </button>
-
-            <iframe
-              className="simplePDF_content"
-              src={`https://embed.simplepdf.eu/editor?open=${children.props.href}`}
-            />
+                <svg
+                  height="512"
+                  viewBox="0 0 512 512"
+                  width="512"
+                  xmlSpace="preserve"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M443.6 387.1 312.4 255.4l131.5-130c5.4-5.4 5.4-14.2 0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4-3.7 0-7.2 1.5-9.8 4L256 197.8 124.9 68.3c-2.6-2.6-6.1-4-9.8-4-3.7 0-7.2 1.5-9.8 4L68 105.9c-5.4 5.4-5.4 14.2 0 19.6l131.5 130L68.4 387.1c-2.6 2.6-4.1 6.1-4.1 9.8 0 3.7 1.4 7.2 4.1 9.8l37.4 37.6c2.7 2.7 6.2 4.1 9.8 4.1 3.5 0 7.1-1.3 9.8-4.1L256 313.1l130.7 131.1c2.7 2.7 6.2 4.1 9.8 4.1 3.5 0 7.1-1.3 9.8-4.1l37.4-37.6c2.6-2.6 4.1-6.1 4.1-9.8-.1-3.6-1.6-7.1-4.2-9.7z" />
+                </svg>
+              </button>
+              <div className="simplePDF_iframeContainer">
+                <iframe
+                  className="simplePDF_iframe"
+                  src={`https://embed.simplepdf.eu/editor?open=${children.props.href}`}
+                />
+              </div>
+            </div>
           </div>,
           document.body
         )}
